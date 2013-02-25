290d4b1e1b8f2548e6caa8c07ccd9878744ee09b
Eugene Wang <eugene.wang.77@gmail.com>
2013-02-25 12:02:10 -0500

    modified student_class.rb

--- student_class.rb
+++ student_class.rb
@@ -1,6 +1,8 @@
 #simplescrape.rb
 require 'open-uri'
 require 'nokogiri'
+require 'pry'
+require 'sqlite3'
 
 url = "http://students.flatironschool.com"   #Insert URL in quotes 
 
@@ -23,11 +25,32 @@ end
 
 #---------------Student Class----------#
 
+db = SQLite3::Database.new("students.db")
+rows = db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY autoincrement,
+    name text,
+    tagline text,
+    bio text,
+    aspirations text,
+    interests text,
+    social_links text,
+    prevwork text,
+    education text,
+    codercred text,
+    fave_apps text,
+    companies text,
+    quotes text
+  );
+SQL
+
 class Student
   attr_accessor :doc, :name, :tagline, :bio, :aspirations, :interests, :social_links, :prevwork, :education, :codercred, :fave_apps, :companies, :quotes
 
+  @@db = SQLite3::Database.new("students.db")
+
   def scrape_name
-  self.name = doc.css("h1").text
+    self.name = doc.css("h1").text
   end
 
   def scrape_tagline
@@ -44,7 +67,7 @@ class Student
      self.interests = doc.css(".two_third p").text
   end
 
-  def social_links
+  def scrape_social_links
     self.social_links = (doc.css("div.social_icons a")).collect {|social_link| social_link.attr("href")}
     # social_links = []
     # self.social_links = []
@@ -87,6 +110,29 @@ class Student
   def scrape_quotes
     self.quotes = doc.css(".center:last").text
   end
+
+  def scrape_all
+    scrape_name
+    scrape_tagline
+    scrape_bio
+    scrape_aspirations
+    scrape_interests
+    scrape_social_links
+    scrape_prevwork
+    scrape_education
+    scrape_codercred
+    scrape_fave_apps
+    scrape_companies
+    scrape_quotes
+  end
+
+  def save
+    @@db.execute(
+        "INSERT INTO students (name, tagline, bio, aspirations, interests, social_links, prevwork, education, codercred, fave_apps, companies, quotes)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+        [name, tagline, bio, aspirations, interests, social_links, prevwork, education, codercred, fave_apps, companies, quotes]);
+  end
+
 end
 
 all_students = []
@@ -100,20 +146,96 @@ student_links.each do |link|
 
   student = Student.new
   student.doc = document
-
+  student.scrape_all
+  student.save
+  binding.pry
 
   all_students << student
 
-  puts student.name
-  puts student.scrape_tagline
-  puts student.scrape_bio
-  puts student.scrape_aspirations
-  puts student.scrape_interests
-  puts student.social_links
-  puts student.scrape_prevwork
-  puts student.scrape_education
-  puts student.scrape_codercred
-  puts student.scrape_fave_apps
-  puts student.scrape_companies
-  puts student.scrape_quotes
+  # puts student.name
+  # puts student.scrape_tagline
+  # puts student.scrape_bio
+  # puts student.scrape_aspirations
+  # puts student.scrape_interests
+  # puts student.scrape_social_links
+  # puts student.scrape_prevwork
+  # puts student.scrape_education
+  # puts student.scrape_codercred
+  # puts student.scrape_fave_apps
+  # puts student.scrape_companies
+  # puts student.scrape_quotes
 end
+
+#### AVI TESTS #####
+
+# def test(title, &b)
+#   begin
+#     if b
+#       result = b.call
+#       if result.is_a?(Array)
+#         puts "fail: #{title}"
+#         puts "      expected #{result.first} to equal #{result.last}"
+#       elsif result
+#         puts "pass: #{title}"
+#       else
+#         puts "fail: #{title}"
+#       end
+#     else
+#       puts "pending: #{title}"
+#     end
+#   rescue => e
+#     puts "fail: #{title}"
+#     puts e
+#   end
+# end
+ 
+# def assert(statement)
+#   !!statement
+# end
+ 
+# def assert_equal(actual, expected)
+#   if expected == actual
+#     true
+#   else
+#     [expected, actual]
+#   end
+# end
+ 
+# test 'should be able to instantiate a student' do
+#   assert Student.new
+# end
+ 
+# test 'should be able to save a student with a name'
+#   s = Student.new
+#   s.name = "Avi Flombaum"
+#   s.save
+ 
+#   assert_equal Student.find_by_name("Avi Flombaum").name, "Avi Flombaum"
+# end
+ 
+# test 'should be able to load all students'
+#   s = Student.new
+#   s.name = "Avi Flombaum"
+#   s.save
+ 
+#   assert Student.all.collect{|s| s.name}.include?("Avi Flombaum")
+# end
+ 
+# test 'should be able to find a student by id'
+#   s = Student.new
+#   s.name = "Avi Flombaum"
+#   s.save
+ 
+#   assert_equal Student.find(s.id).name, "Avi Flombaum"
+# end
+ 
+# test 'should be able to update a student'
+#   s = Student.new
+#   s.name = "Avi Flombaum"
+#   s.save
+ 
+#   s.name = "Bob Whitney"
+#   s.save
+ 
+#   assert_equal Student.find(s.id).name, "Bob Whitney"
+# end
